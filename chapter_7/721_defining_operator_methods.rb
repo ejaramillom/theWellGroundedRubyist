24ca129ec29fa2f5598148ca0c537ae39062f458
ejaramillom <ejaramilo@hotmail.com>
2023-04-08 20:44:10 +0000

    722 customize unary operators

---
+++ chapter_7/721_defining_operator_methods.rb
@@ -0,0 +1,31 @@
+obj = Object.new
+def obj.+(other_obj)
+  "Trying to add something to me, eh?"
+end
+
+puts obj + 100 # no addition, just output
+
+# bank account with + and - methods
+
+class Account
+  attr_accessor :balance
+  def initialize(amount=0)
+    self.balance = amount
+  end
+  
+  def +(x)
+    self.balance += x
+  end
+  
+  def –(x)
+    self.balance -= x
+  end
+  
+  def to_s
+    balance.to_s
+  end
+end
+
+acc = Account.new(20)
+acc -= 5
+puts acc
---
+++ chapter_7/72_syntactic_sugar_methods.rb
@@ -0,0 +1,38 @@
+Category
+Name 
+Definition example 
+Calling example 
+Sugared notation
+
+Arithmetic method/operators
+
++ def +(x) obj.+(x) obj + x
+- def -(x) obj.-(x) obj - x
+* def *(x) obj.*(x) obj * x
+/ def /(x) obj./(x) obj / x
+% def %(x) obj.%(x) obj % x
+
+Get/set/append data
+  
+[]  def [](x)    obj.[](x)    obj[x]
+[]= def []=(x,y) obj.[]=(x,y) obj[x] = y
+<<  def <<(x)    obj.<<(x)    obj << x
+  
+Comparison method/ operators
+  
+== def ==(x) obj.==(x) obj == x
+>  def >(x)  obj.>(x)  obj > x
+<  def <(x)  obj.<(x)  obj < x
+>= def >=(x) obj.>=(x) obj >= x
+<= def <=(x) obj.<=(x) obj <= x
+  
+Case equality operator
+
+=== def ===(x) obj.===(x) obj === x
+  
+Bit-wise operators
+
+| def |(x) obj.|(x) obj | x
+& def &(x) obj.&(x) obj & x
+^ def ^(x) obj.^(x) obj ^ x
+  
