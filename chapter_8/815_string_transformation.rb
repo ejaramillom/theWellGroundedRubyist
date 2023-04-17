64ce3fa8df497d1d9e711ce6754339d9acd8bec9
ejaramillom <ejaramilo@hotmail.com>
2023-04-17 15:51:13 +0000

    815 string transformation start

---
+++ chapter_8/815_string_transformation.rb
@@ -0,0 +1,20 @@
+# case transformation
+
+string = 'David A. Black'
+
+string.upcase
+# => "DAVID A. BLACK"
+
+string.downcase
+# => "david a. black"
+
+string.swapcase
+# => "dAVID a. bLACK"
+
+string = 'david'
+
+string.capitalize # or capitalize!
+# => 'David'
+
+# formatting
+
