1ef66607dffb83a75cdea7267484792196b74a83
ejaramillom <ejaramilo@hotmail.com>
2023-04-18 20:56:12 +0000

    815 string transformation

--- chapter_8/815_string_transformation.rb
+++ chapter_8/815_string_transformation.rb
@@ -16,5 +16,51 @@ string = 'david'
 string.capitalize # or capitalize!
 # => 'David'
 
-# formatting
+# formatting transformations
+
+string.rjust(5) # puts extra spaces
+# => '     David A. Black'
+
+string.ljust(5)
+# => 'David A. Black     '
+
+string.rjust(5, '.')
+# => '.....David A. Black'
+
+string.center(25)
+# => '     David A. Black     '
+
+string = '   David A. Black   '
+
+string.strip # rstrip and lstrip
+# => 'David A. Black'
+
+# all of them have bang methods
+
+# content transformation
+
+string.chop
+# => 'David A. Blac'
+
+'David A. Black\n'.chomp # the default target of chomp is \n
+# => 'David A. Black'
+
+string.chomp('ck')
+# => 'David A. Bla'
+
+string.clear # permanently changes the string
+# => ''
+
+string.replace('to be named later') # permanently changes the string
+# => string -> 'to be named later'
+
+string.delete('abc') # delete receives the same arguments as .count
+# => 'Dvid A. blk'
+
+string.crypt('34')
+# => '3470EY.7YRmio'
+
+'a'.succ # finds next highest string
+# => 'b'
+
 
