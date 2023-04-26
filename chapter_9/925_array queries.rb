764054d19b40f2a9f031faaeccf3a1235267569f
ejaramillom <ejaramilo@hotmail.com>
2023-04-26 16:48:28 +0000

    925 array queries

---
+++ chapter_9/925_array queries.rb
@@ -0,0 +1,9 @@
+Method name/sample call                Meaning
+
+a.size (synonyms: length, count)       Number of elements in the array
+a.empty?                               True if a is an empty array; false if it has any elements282
+a.include?(item)                       True if the array includes item; false, otherwise
+a.count(item)                          Number of occurrences of item in array
+a.first(n=1)                           First n elements of array
+a.last(n=1)                            Last n elements of array
+a.sample(n=1)                          n random elements from array
