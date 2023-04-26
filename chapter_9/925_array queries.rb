427a54cedff7a52061412f3ccb864890073c549a
ejaramillom <ejaramilo@hotmail.com>
2023-04-26 20:14:13 +0000

    931 creating hashes

--- chapter_9/925_array queries.rb
+++ chapter_9/925_array queries.rb
@@ -1,7 +1,7 @@
 Method name/sample call                Meaning
 
 a.size (synonyms: length, count)       Number of elements in the array
-a.empty?                               True if a is an empty array; false if it has any elements282
+a.empty?                               True if a is an empty array; false if it has any elements
 a.include?(item)                       True if the array includes item; false, otherwise
 a.count(item)                          Number of occurrences of item in array
 a.first(n=1)                           First n elements of array
---
+++ chapter_9/931_creating_hashes.rb
@@ -0,0 +1,5 @@
+h = {} # literal hash
+
+h = Hash.new # constructor
+
+h = Hash['Connecticut', 'CT', 'Delaware', 'DW'] # class method
