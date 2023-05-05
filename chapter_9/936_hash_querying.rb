9dbf558df151e458c98933a7943a5e38cd7b1bb5
ejaramillom <ejaramilo@hotmail.com>
2023-05-05 16:17:44 +0000

    936 hash querying

---
+++ chapter_9/936_hash_querying.rb
@@ -0,0 +1,10 @@
+Method name/Sample call      Meaning
+
+h.has_key?(1)                True if h has the key 1
+h.include?(1)                Synonym for has_key?
+h.key?(1)                    Synonym for has_key?
+h.member?(1)                 Synonym for has_key?
+h.has_value?("three")        True if any value in h is "three"
+h.value?("three")            Synonym for has_value?
+h.empty?                     True if h has no key/value pairs
+h.size                       Number of key/value pairs in h
