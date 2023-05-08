174bb17d7328f435c9138daddaec7ab020281e07
ejaramillom <ejaramilo@hotmail.com>
2023-05-08 20:16:10 +0000

    1041 enumerable elements operations

---
+++ chapter_10/1041_enumerable_elements_operations.rb
@@ -0,0 +1,11 @@
+[1,2,3,4].first
+# => 1
+
+(1..10).first
+# => 1
+
+{1 => 2, "one two" => "three"}.first
+# => [1, 2]
+
+# last method
+# last only exists for arrays and ranges, on possible endless each, it makes no sense, and does not exist
