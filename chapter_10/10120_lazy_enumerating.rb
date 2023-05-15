4dc355af4e702955f23f14b7e2bce31145922e63
ejaramillom <ejaramilo@hotmail.com>
2023-05-15 21:16:10 +0000

    10120 lazy fizzbuzz

--- chapter_10/10120_lazy_enumerating.rb
+++ chapter_10/10120_lazy_enumerating.rb
@@ -1,2 +1,23 @@
 (1..Float::INFINITY).lazy.select {|n| n % 3 == 0 }.first(10) # this code does not run forever because of the lazy in infinity
 # => [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
+
+# fizzbuzz
+
+def fb_calc(i)
+  case 0
+  when i % 15
+    "FizzBuzz"
+  when i % 3
+    "Fizz"
+  when i % 5
+    "Buzz"
+  else
+    i.to_s
+  end
+end
+
+def fb(n)
+  (1..Float::INFINITY).lazy.map {|i| fb_calc(i) }.first(n)
+end
+
+p fb(15)
