c556a3f39128285b4b4ffc07ebbf32c32586102e
ejaramillom <ejaramilo@hotmail.com>
2023-06-05 15:11:18 +0000

    1512 included and prepended

---
+++ chapter_15/1512_included_and_prepended.rb
@@ -0,0 +1,32 @@
+module M
+  def self.included(c)
+    puts "I have just been mixed into #{c}."
+  end
+end
+class C
+  include M
+end
+
+# => I have just mixed into C
+
+# the callback throws a message when the module is included
+
+# add new methods when module is included in another one
+
+module M
+  def self.included(cl)
+    def cl.a_class_method
+      puts "Now the class has a new class method."
+    end
+  end
+  def an_inst_method
+    puts "This module supplies this instance method."
+  end
+end
+class C
+  include M
+end
+
+C.a_class_method
+c = C.new
+c.an_inst_method
