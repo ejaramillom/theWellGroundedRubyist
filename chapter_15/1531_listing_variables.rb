f8e679ab188db652741dafe4b490beed9975d4b6
ejaramillom <ejaramilo@hotmail.com>
2023-06-06 14:58:48 +0000

    1531 listing variables

---
+++ chapter_15/1531_listing_variables.rb
@@ -0,0 +1,5 @@
+x = 1
+p local_variables
+# => [:x]
+p global_variables.sort
+# => [:$!, :$", :$$, :$&, :$', :$*, :$+, :$,, :$-0, :$-F, :$-I, :$-K, :$-W, :$-a, :$-d, :$-i, :$-l, :$-p, :$-v, :$-w, :$., :$/, :$0, :$:, :$;, :$<, :$=, :$>, :$?, :$@, :$DEBUG, :$FILENAME, :$KCODE, :$LOADED_FEATURES, :$LOAD_PATH, :$PROGRAM_NAME, :$SAFE, :$VERBOSE, :$\, :$_, :$`, :$binding, :$stderr, :$stdin, :$stdout, :$~]
