c5bd45084195fe6e577a018c554409116c80cac7
ejaramillom <ejaramilo@hotmail.com>
2023-06-06 14:02:59 +0000

    1515 module const missing

---
+++ chapter_15/1515_module_const_missing.rb
@@ -0,0 +1,12 @@
+class C
+  def self.const_missing(const)
+    puts "#{const} is undefined—setting it to 1."
+    const_set(const,1)
+  end
+end
+puts C::A
+puts C::A
+
+# => A is undefined—setting it to 1.
+# => 1
+# => 1
